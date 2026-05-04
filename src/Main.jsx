8c527aafad37836378cb29ed63b75b45a8b186a5
shlok612 <shlokkatiyar62@gmail.com>
2026-05-05 00:52:28 +0530

    footer for frontend updated

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Room from "./Room.jsx";
+import About from "./pages/About.jsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
+import Terms from "./pages/Terms.jsx";
+import Contact from "./pages/Contact.jsx";
 import Footer from "./components/layout/Footer";
 import "./index.css";
 
@@ -11,6 +15,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/room/:code" element={<Room />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
     <Footer />
   </BrowserRouter>
